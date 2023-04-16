11625a515ad4d540900c54c4463fc0487424c566
mannco1 <70008207+mannco1@users.noreply.github.com>
2023-04-16 14:44:16 +0300

    Add files via upload

---
+++ pr8/Header.h
@@ -0,0 +1,17 @@
+class type3 {
+private:
+	double C;
+public:
+	type3(double c1);
+	void Get_answer();
+	void show();
+};
+
+class type4 {
+private:
+	double A, C;
+public:
+	type4(double a1, double c1);
+	void Get_answer();
+	void show();
+};
---
+++ pr8/mainpr8-1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+#include "Header.h"
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	type3 t3(5);
+	type4 t4(2, -8);
+
+	
+	t3.show();
+	t4.show();
+
+	t3.Get_answer();
+	t4.Get_answer();
+
+	
+	return 0;
+}
